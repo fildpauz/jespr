6de60a46ccbb63b7600081e056acf19f5cc04bc5
Ralph Rose <rose@waseda.jp>
2016-07-13 16:20:46 +0900

    Create self-paced-reading.js

---
+++ self-paced-reading.js
@@ -0,0 +1,37 @@
+/*
+ * Title: self-paced-reading.js
+ * Author: Ralph L. ROSE
+ * E-mail address: rose@waseda.jp
+ * Description: This javascript library is designed to support the
+ * execution of self-paced reading experiments via a web browser.
+ */
+
+/*
+ * Script-wide variables
+ */
+var exptTitle;      // Title of experiment
+var exptFrame;      // Top level div container for experiment display
+var exptFontname;   // Font to be used for main display elements
+var exptFontsize;   // Base font size to be used for main display elements
+
+/*
+ * Load experimental design and stimuli
+ */
+function loadExperiment(){
+}
+
+/*
+ * Initialize experimental display
+ */
+function initExperiment(){
+  // create exptFrame
+  // create elements of exptFrame
+  // populate elements with initial values
+  // show title
+}
+
+/*
+ * Display an error box with an error message
+ */
+function displayErrorMessage(message){
+}
