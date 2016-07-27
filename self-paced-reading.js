402a6cc0875d1a58c61eff1ad662fe3bc42bf863
Ralph Rose <rose@waseda.jp>
2016-07-27 23:14:15 +0900

    Updated javascript library

--- self-paced-reading.js
+++ self-paced-reading.js
@@ -9,6 +9,8 @@
 /*
  * Script-wide variables
  */
+var exptDesign;     // json object containing the design, stimuli, etc.
+var designValidated = false;    // Boolean to indicate whether design file has been validated
 var exptTitle = "No title";      // Title of experiment
 var exptFontname = "Courier new";   // Font to be used for main display elements
 var exptFontsize = "12";   // Base font size to be used for main display elements
@@ -21,6 +23,8 @@ var exptMaskchar = "_";     // Char to use as masking character during trials
 var exptFixationchar = "+";     // Char to use as fixation symbol between trials
 var exptForm;       // HTML form object that will contain and return the experimental data. Must be named "SPRForm".
 var exptFrame;      // Top level div container for experiment display
+var screenInfo = [];     // List of all screen divs in the experiment: title, instructions, stimulus items
+var curScreenIndex; // The index of the current screen in screenInfo array being displayed.
 
 /*
  * A function to run the whole experiment from initialization to administration
@@ -28,13 +32,35 @@ var exptFrame;      // Top level div container for experiment display
  */
 function doExperiment(){
     //loadExperiment();
-    initExperiment();
+    initDisplay();
+    curScreenIndex = 0;
+    showNextScreen(curScreenIndex);
 }
 
 /*
- * Load experimental design and stimuli
+ * Displays the screen indexed in screenInfo indexed by the param.
+ * @param index indicating the index of the screen in screenInfo array to display
+ */
+function showNextScreen(index){
+    var nextDiv = screenInfo[index]["div"];
+    exptFrame.appendChild(nextDiv);
+    nextDiv.style.display = "block";
+}
+
+/*
+ * Load general experimental parameters and validate json object describing design
+ * @returns a boolean to indicate whether json object is valid (true) or not.
  */
 function loadExperiment(design){
+    if (!designValidated) {
+        sprLog("Loading a non-validated design! Be sure to call isValidDesign() on design object first");
+        exptDesign = design;
+    } else if (design != exptDesign) {
+        sprLog("Loading a design different than the one previously validated!");
+        exptDesign = design;
+    } else {
+        sprLog("Loading design object");
+    }
     setTitle(design);
     setFontname(design);
     setFontsize(design);
@@ -43,44 +69,218 @@ function loadExperiment(design){
     setDisplay(design);
     setMaskchar(design);
     setFixationchar(design);
+    // Populate the screenInfo array with the sequence of screens for the whole experiment
+    screenInfo.push(getTitleScreenInfo(design));
+    screenInfo.concat(getInstructionScreensInfo(design["instruction-screens"]));
+    screenInfo.concat(getStimuliGroupScreensInfo(design["practice-stimuli"]));
+    screenInfo.concat(getInstructionScreensInfo(design["post-practice-instruction-screens"]));
+    screenInfo.concat(getStimuliSetsScreensInfo(design["experiment-stimuli"]));
 }
 
 /*
- * Initialize experimental display
+ * Gets the title and investigator info from the json design object and returns
+ * an object that represents info for the experiment title screen.
+ * @param   design - the json experiment design object
+ * @returns {undefined}
  */
-function initExperiment(){
-  // create exptFrame
-  exptForm = document.getElementsByName("SPRForm")[0];
-  if (exptForm === 'undefined'){
-      sprLog("The HTML document does not contain a form named \'SPRForm\'");
-      displayErrorMessage("The HTML document does not contain a form named \'SPRForm\'. Discontinuing experiment.");
-  }
-  exptFrame = document.createElement("div");
-  exptFrame.className = "experiment-frame";
-  exptForm.appendChild(exptFrame);
-  // create title screen
-  var titleDiv = createTitleDiv();
-  exptFrame.appendChild(titleDiv);
-  // create elements of exptFrame
-  // populate elements with initial values
-  // show title
-  titleDiv.style.display = "block";
- }
-
-/*
- * Creates a div element and populates it with html containing the
- * experiment title and names of investigators
- * @returns a div element containing title and other info
- */
-function createTitleDiv(){
+function getTitleScreenInfo(design){
     // create the title div
     var titleDiv = document.createElement("div");
     titleDiv.className = "title";
     // create and add the title
     var title = document.createElement("h1");
-    title.textContent = exptTitle;
+    title.textContent = exptTitle; // Is it necessary to have the exptTitle object?
     titleDiv.appendChild(title);
-    // TODO: create and add the investigators's names
+    // create and add investigator info
+    var investigators = document.createElement("p");
+    investigators.className = "investigators";
+    var pi = ""; // primary investigator list
+    var oi = ""; // other investigator list
+    for (var i=0; i<design["investigators"].length; i++){
+        if (typeof(design["investigators"][i]) === 'primary') {
+            if (pi.length > 0) { pi = pi.concat(", "); }
+            pi = pi.concat("<b>", design["investigators"][i]["primary"], "</b>");
+        } else if (typeof(design["investigators"][i]) === 'other') {
+            if (oi.length > 0) { oi = oi.concat(", "); }
+            oi = oi.concat(design["investigators"][i]["other"]);
+        } else {
+            sprLog("Unknown type of investigator in investigator list: 'primary'/'other' expected.");
+        }
+    }
+    investigators.textContent = pi.concat(" ").concat(oi).trim();
+    titleDiv.appendChild(investigators);
+    var titleScreenInfo = { "type" : "title" };
+    titleScreenInfo["div"] = titleDiv;
+    return titleScreenInfo;
+}
+
+/*
+ * Gets instructions info from the json design object and returns
+ * an array that represents info for the screen(s).
+ * @param   design - the json instructions design object
+ * @returns {undefined}
+ */
+function getInstructionScreensInfo(design){
+    var screensInfo = [];
+    for (var i=0; i<design.length; i++){
+        // Create the div container for the instructions
+        var instructionsDiv = document.createElement("div");
+        instructionsDiv.textContent = design[i]["instruction-screen"];
+        // Create the screenInfo object and push it to the array
+        var screenInfo = { "type": "instructions" };
+        screenInfo["div"] = instructionsDiv;
+        screensInfo.push(screenInfo);
+    }
+    return screensInfo;
+}
+
+/*
+ * Gets item screens info from the json design object and returns
+ * an array that represents info for the screen(s).
+ * @param   design - the json stimuli group design object
+ * @returns array of items
+ */
+function getStimuliGroupScreensInfo(design){
+    var screensInfo = [];
+    var order = getOrder(design["order"]);
+    // go through items array and create screenInfo object for each item
+    for (var i=0; i<design["items"].length; i++){
+        // Create the div container for the item
+        var itemDiv = document.createElement("div");
+        var itemId = design["items"][i]["id"];
+        itemDiv.id = itemId;
+        var regionsInfo = []; // the info array for the regions
+        if (design["items"][i]["string"]){ // single-line SPR type
+            // split string into regions
+            var regions = design["items"][i]["string"].split("|");
+            // for each region
+            for (var i=0; i<regions.length; i++){
+                // create a regionInfo object with region ID and region text
+                var regionInfo = { "id": itemId.concat("_").concat(i),
+                                   "string": region[i].trim() };
+                // add regionInfo to regionsInfo array
+                regionsInfo.push(regioninfo);
+                // create a span with region ID and text content with masking char
+                var regionSpan = document.createElement("span");
+                regionSpan.id = regionInfo["id"];
+                regionSpan.className = "sprRegion";
+                regionSpan.textContent = regionInfo["string"].replace(/./g, exptMaskchar);
+                // add span to div
+                if (i>0){
+                    var space = document.createTextNode(" ");
+                    itemDiv.appendChild(space);
+                }
+                itemDiv.appendChild(regionSpan);
+            }
+        } else if (design["items"][i]["strings"]){ // multi-line SPR type
+            // TODO: implement the multi-line strings handler
+        }
+        // Create the screenInfo object and push it to the array
+        var screenInfo = { "type": "item" };
+        screenInfo["div"] = itemDiv;
+        screensInfo.push(screenInfo);
+    }
+    if (order === "random"){
+        shuffle(screensInfo);
+    }
+    return screensInfo;
+}
+
+/*
+ * Gets item screens info from the json design object and returns
+ * an array that represents info for the screen(s).
+ * @param   design - the json stimuli group design object
+ * @returns array of items
+ */
+function getStimuliSetsScreensInfo(design){
+    var screensInfo = [];
+    var order = getOrder(design["order"]);
+    
+    return screensInfo;
+}
+
+/*
+ * Determines the "order" value or defaults to "fixed" if undefined
+ * @param   object containing "order" key-value pair
+ * @returns "fixed" or "random"
+ */
+function getOrder(order){
+    var result = "fixed"; // default
+    if (order !== 'undefined'){
+        if (order["order"] == "random"){
+            result = "random";
+        } else if (order["order"] != "fixed"){
+            sprLog("Unexpected value for 'order'. Using default 'fixed'.");
+        }
+    }
+    return result;
+}
+
+/*
+ * Initialize experimental display
+ */
+function initDisplay(){
+  // create exptFrame
+  exptForm = document.getElementsByName("SPRForm")[0];
+  if (exptForm === 'undefined'){
+      displayErrorMessage("The HTML document does not contain a form named \'SPRForm\'. Discontinuing experiment.");
+      sprLog("The HTML document does not contain a form named \'SPRForm\'");
+      return;
+  }
+  exptFrame = document.createElement("div");
+  exptFrame.className = "experiment-frame";
+  exptFrame.addEventListener("keypress", processKeyPress);
+  exptForm.appendChild(exptFrame);
+  // create title screen
+//  var titleDiv = createTitleDiv();
+//  exptFrame.appendChild(titleDiv);
+  // create instructions frame
+  // create elements of exptFrame
+  // populate elements with initial values
+  // show title
+//  titleDiv.style.display = "block";
+ }
+
+/*
+ * Create a key listener to handle all key press
+ * events and advance the experiment.
+ */
+function processKeyPress(e){
+    var keyCode = e.keyCode;
+    alert(keyCode);
+}
+
+/*
+ * Creates a div element and populates it with html containing the
+ * experiment title and names of investigators
+ * @param   design - the json experiment design object
+ * @returns a div element containing title and other info
+ */
+function createTitleDiv(design){
+    // create the title div
+    var titleDiv = document.createElement("div");
+    titleDiv.className = "title";
+    // create and add the title
+    var title = document.createElement("h1");
+    title.textContent = design["title"];
+    titleDiv.appendChild(title);
+    var investigators = document.createElement("p");
+    investigators.className = "investigators";
+    var pi = ""; // primary investigator list
+    var oi = ""; // other investigator list
+    for (var i=0; i<design["investigators"].length; i++){
+        if (typeof(design["investigators"][i]) === 'primary') {
+            if (pi.length > 0) { pi = pi.concat(", "); }
+            pi = pi.concat("<b>", design["investigators"][i]["primary"], "</b>");
+        } else if (typeof(design["investigators"][i]) === 'other') {
+            if (oi.length > 0) { oi = oi.concat(", "); }
+            oi = oi.concat(design["investigators"][i]["other"]);
+        } else {
+            sprLog("Unknown type of investigator in investigator list: 'primary'/'other' expected.");
+        }
+    }
+    investigators.textContent = pi.concat(" ").concat(oi).trim();
+    titleDiv.appendChild(investigators);
     return titleDiv;
 }
 
@@ -228,4 +428,198 @@ function validTextColour(stringToTest) {
     image.style.color = "rgb(255, 255, 255)";
     image.style.color = stringToTest;
     return image.style.color !== "rgb(255, 255, 255)";
+}
+
+function isValidDesign(design){
+    exptDesign = design;
+    sprLog("Validating design");
+    var result = true;
+    // Check structure of pre-practice instructions
+    sprLog("Checking pre-practice instruction screens");
+    if (design["instruction-screens"]){
+        if (!isValidInstructionScreen(design["instruction-screens"])){
+            result = false;
+        }
+    } else {
+        sprLog("No pre-practice instruction screens");
+    }
+    // Check structure of practice items
+    sprLog("Checking practice stimuli");
+    if (design["practice-stimuli"]){
+        if (!isValidGroup(design["practice-stimuli"])){
+            result = false;
+        }
+    } else {
+        sprLog("No practice stimuli");
+    }
+    // Check structure of post-practice instructions
+    sprLog("Checking post-practice instruction screens");
+    if (design["post-practice-instruction-screens"]){
+        if (!isValidInstructionScreen(design["post-practice-instruction-screens"])){
+            result = false;
+        }
+    } else {
+        sprLog("No post-practice instruction screens");
+    }
+    // Check structure of experimental stimuli
+    sprLog("Checking experimental-stimuli");
+    if (design["experiment-stimuli"]){
+        if (!isValidExptStimuli(design["experiment-stimuli"])){
+            result = false;
+        }
+    } else {
+        displayErrorMessage("No experimental stimuli section was found in the json object");
+        sprLog("No experimental stimuli section was found in the json object");
+        result = false;
+    }
+    return result;
+}
+
+function isValidExptStimuli(stimuli){
+    var result = true;
+    if (!stimuli["stimuli-sets"]){
+        displayErrorMessage("No 'stimuli-sets' name-value pair in experimental stimuli section");
+        sprLog("No 'stimuli-sets' name-value pair in experimental stimuli section");
+        result = false;
+    } else if (stimuli["stimuli-sets"].length < 1){
+        displayErrorMessage("No 'stimuli-set' found in experimental stimuli sets section");
+        sprLog("No 'stimuli-set' found in experimental stimuli sets section");
+        result = false;
+    } else {
+        for (var i=0; i<stimuli["stimuli-sets"].length; i++){
+            if (typeof(stimuli["stimuli-sets"][i]["stimuli-set"]) === 'undefined') {
+                displayErrorMessage("Unknown name in stimuli: expected 'stimuli-set'");
+                sprLog("Unknown name in stimuli: expected 'stimuli-set'");
+                result = false;
+            } else if (!isValidStimuliSet(stimuli["stimuli-sets"][i]["stimuli-set"])){
+                result = false;
+            }
+        }
+    }
+    return result;
+}
+
+function isValidStimuliSet(stimuliSet){
+    var result = true;
+    if (!stimuliSet["groups"]){
+        displayErrorMessage("No 'group' name-value pair in experimental stimuli section");
+        sprLog("No 'group' name-value pair in experimental stimuli section");
+        result = false;
+    } else if (stimuliSet["groups"].length < 1){
+        displayErrorMessage("No stimuli 'group' found in experimental stimuli section");
+        sprLog("No stimuli 'group' found in experimental stimuli section");
+        result = false;
+    } else {
+        for (var i=0; i<stimuliSet["groups"].length; i++){
+            if (typeof(stimuliSet["groups"][i]["group"]) === 'undefined') {
+                displayErrorMessage("Unknown name in stimuli: expected 'group'");
+                sprLog("Unknown name in stimuli: expected 'group'");
+                result = false;
+            } else if (!isValidGroup(stimuliSet["groups"][i]["group"])){
+                result = false;
+            }
+        }
+    }
+    return result;
+}
+
+function isValidInstructionScreen(instructions){
+    var result = true;
+    for (var i=0; i<instructions.length; i++){
+        if (typeof(instructions[i]["instruction-screen"]) === 'undefined'){
+//            console.log(instructions[i]);
+            displayErrorMessage("Unknown name in instructions: expected 'instruction-screen'");
+            sprLog("Unknown name in instructions: expected 'instruction-screen'");
+            result = false;
+        } else if (instructions[i].length < 1){ // Will this allow an array?
+            displayErrorMessage("Empty string in instructions");
+            sprLog("Empty string in instructions");
+            result = false;
+        }
+    }
+    return result;
+}
+
+function isValidGroup(group){
+    var result = true;
+    if (!group["items"]){
+        displayErrorMessage("No 'items' name-value pair in stimuli");
+        sprLog("No 'items' name-value pair in stimuli");
+        result = false;
+    } else if (group["items"].length < 1){
+        displayErrorMessage("No 'item' found in items list");
+        sprLog("No 'item' found in items list");
+        result = false;
+    } else {
+        for (var i=0; i<group["items"].length; i++){
+            if (typeof(group["items"][i]["item"]) === 'undefined'){
+                displayErrorMessage("Unknown name in items: expected 'item'");
+                sprLog("Unknown name in items: expected 'item'");
+                result = false;
+            } else if (!isValidItem(group["items"][i]["item"])){
+                result = false;
+            }
+        }
+    }
+    return result;
+}
+
+function isValidItem(item){
+    var result = true;
+    if (!item["id"]){
+        displayErrorMessage("Stimulus item has no id");
+        sprLog("Stimulus item has no id");
+        result = false;
+    } else {
+        sprLog("Checking item: " + item["id"]);
+        if (!item["strings"]){
+            if (!item["string"]){
+                displayErrorMessage("Item has no string(s)!");
+                sprLog("Item has no string(s)!");
+                result = false;
+            } else if (item["string"].length < 1){
+                displayErrorMessage("Empty string found!");
+                sprLog("Empty string found!");
+                result = false;
+            }
+        } else {
+            for (i=0; i<item["strings"].length; i++){
+                if (typeof(item["strings"][i]["string"]) === 'undefined'){
+                    displayErrorMessage("Unknown name in strings: expected 'string'");
+                    sprLog("Unknown name in strings: expected 'string'");
+                    result = false;
+                } else if (item["strings"][i]["string"].length < 1) {
+                    displayErrorMessage("Empty string found!");
+                    sprLog("Empty string found!");
+                    result = false;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+/*
+ * Shuffles an array object in place and returns the object
+ * Credit: https://github.com/coolaj86/knuth-shuffle
+ * @param an array
+ * @return the shuffled array
+ */
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
