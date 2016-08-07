a64f4630041a3630e5ca5d0958c32a9d6d6c73d0
Ralph Rose <rose@waseda.jp>
2016-08-07 16:03:03 +0900

    Created full draft of spr-objects.js (not debugged yet)

--- spr-objects.js
+++ spr-objects.js
@@ -33,9 +33,9 @@ function Region(id, text, index, location, item){
 /*
  * Changes the currently displayed text content to masked text of equal length
  */
-Region.prototype.mask = function(){
-    var maskChar = exptMaskchar === 'undefined' ? "_" : exptMaskchar;
-    this.html.textContent = text.replace(/./g, maskChar);
+Region.prototype.mask = function(char){
+    var maskChar = typeof char === 'undefined' ? "!" : char;
+    this.html.textContent = this.text.replace(/./g, maskChar);
     return this;
 };
 
@@ -75,10 +75,16 @@ Region.prototype.createHtml = function(){
  * are presented horizontally (i.e., one-sentence stimuli) or vertically (i.e.,
  * multi-sentence stimuli). [default=true]
  */
-function Item(id, text, isHorizontal){
-    if (isHorizontal === 'undefined') { isHorizontal = true; }
+function Item(id, text, orientation, fixationChar, maskChar){
     this.id = id;
     this.text = text; // Is it useful to store this as plain text: .replace(/\|/g, ' ') ?
+    if (orientation === 'horizontal' | orientation === 'vertical') {
+        this.orientation = orientation;
+    } else {
+        this.orientation = "horizontal"; // default to horizontal
+    }
+    this.fixationChar = fixationChar;
+    this.maskChar = maskChar;
     this.regions = this.parseRegions();
     this.curRegionIndex = 'undefined';   // The index of the current SPR region being displayed
     this.prompt = 'undefined';  // The prompt and options variables
@@ -87,6 +93,7 @@ function Item(id, text, isHorizontal){
     this.feedback = 'undefined';
     this.condition = []; // An array of values representing the experimental conditions
     this.html = this.createHtml();
+    this.timeData = [];
 }
 
 Item.prototype.show = function(frame){
@@ -106,40 +113,91 @@ Item.prototype.processKeypress = function(keyCode, elapsedTime){
     switch (keyCode){
         case 32: // space bar
             if (this.curRegionIndex === -1){ // fixation mark is showing
-
+                var fixationP = document.getElementById(this.id + "-fixation");
+                fixationP.style.display = "none";
+                var stimulusP = document.getElementById(this.id + "-stimulus");
+                stimulusP.style.display = "block";
+                curRegionIndex++;
+                this.saveData(this.id + "-fixation", elapsedTime, keyCode);
             } else if (this.curRegionIndex < this.regions.length){ // non-final SPR region is showing
-
+                var curRegion = regions[curRegionIndex];
+                var nextRegion = regions[curRegionIndex+1];
+                curRegion.mask(this.maskChar);
+                nextRegion.unmask();
+                curRegionIndex++;
+                this.saveData(curRegion.id, elapsedTime, keyCode);
             } else if (this.curRegionIndex === this.regions.length-1){ // final SPR region is showing
-                this.hide();
-                result = "end of screen";
-            } else if (this.curRegionIndex === this.regions.length+1){ // prompt is showing
-                this.hide();
-                result = "end of screen";
+                var curRegion = regions[curRegionIndex];
+                this.saveData(curRegion.id, elapsedTime, keyCode);
+                curRegionIndex++;
+                if (typeof this.prompt !== 'undefined'){
+                    var stimulusP = document.getElementById(this.id + "-stimulus");
+                    stimulusP.style.display = "none";
+                    var promptP = document.getElementById(this.id + "-prompt");
+                    promptP.style.display = "block";
+                } else {
+                    this.hide();
+                    result = "end of screen";
+                }
+            } else if (this.curRegionIndex === this.regions.length){ // prompt is showing
+                // prompt is showing, but non-answer key pressed -- ignore
             } else if (this.curRegionIndex === this.regions.length+1){ // feedback is showing
+                this.saveData(this.id + "-feedback", elapsedTime, keyCode)
                 this.hide();
                 result = "end of screen";
-            } else { // prompt is showing, but other key pressed -- ignore
+            } else { // This case should never be reached.
             }
             break;
-        case 48:      // digit 0
-        case 112, 80: // p,P
-        case 108, 76: // l,L
-        case 109, 77: // m,M
-            this.hide();
-            result = "end of screen";
-            break;
+        // Option 1 buttons (on left-hand side of keyboard)
         case 49:      // digit 1
         case 113, 81: // q,Q
         case 97, 65:  // a,A
         case 122, 90: // z,Z
-            this.hide();
-            result = "end of screen";
+            this.saveData(this.id + "-prompt", elapsedTime, keyCode);
+            if (typeof this.feedback !== 'undefined'){
+                var promptP = document.getElementById(this.id + "-prompt");
+                promptP.style.display = "none";
+                var feedbackP = document.getElementById(this.id + "-feedback");
+                // TODO: Check whether response is correct and update
+                // feedbackP textContent as appropriate
+                feedbackP.style.display = "block";
+                curRegionIndex++;
+            } else {
+                this.hide();
+                result = "end of screen";
+            }
+            break;
+        // Option 2 buttons (on right-hand side of keyboard)
+        case 48:      // digit 0
+        case 112, 80: // p,P
+        case 108, 76: // l,L
+        case 109, 77: // m,M
+            this.saveData(this.id + "-prompt", elapsedTime, keyCode);
+            if (typeof this.feedback !== 'undefined'){
+                var promptP = document.getElementById(this.id + "-prompt");
+                promptP.style.display = "none";
+                var feedbackP = document.getElementById(this.id + "-feedback");
+                // TODO: Check whether response is correct and update
+                // feedbackP textContent as appropriate
+                feedbackP.style.display = "block";
+                curRegionIndex++;
+            } else {
+                this.hide();
+                result = "end of screen";
+            }
             break;
         default:
             // Pressed other key -- do nothing
     }
     return result;
 };
+
+Item.prototype.saveData = function(regionId, elapsedTime, keyCode){
+    var data = { "regionId": regionId,
+                 "elapsedTime": elapsedTime,
+                 "keyCode": keyCode };
+    this.timeData.push(data);
+}
 
 /*
  * 
@@ -153,9 +211,9 @@ Item.prototype.createHtml = function(){
     var fixationP = document.createElement("p");
     fixationP.id = this.id.concat("-fixation");
     fixationP.className = "fixation";
-    fixationP.textContent = exptFixationchar;
+    fixationP.textContent = this.Fixationchar;
     itemDiv.appendChild(fixationP);
-    if (this.isHorizontal){ // single-line SPR type
+    if (this.orientation === 'horizontal'){ // single-line SPR type
         // split string into regions
         var itemP = document.createElement("p"); // create p container for regions
         itemP.id = this.id.concat("-stimulus");
@@ -167,13 +225,13 @@ Item.prototype.createHtml = function(){
                 var space = document.createTextNode(" ");
                 itemP.appendChild(space);
             }
-            itemP.appendChild(regions[i].mask().html);
+            itemP.appendChild(regions[i].mask(this.maskChar).html);
         }
         itemDiv.appendChild(itemP);
     } else { // multi-line SPR type
         // TODO: create a multi-line version
     }
-    // TODO: Create and append the prompt <div>, if any
+    // TODO: Create and append the prompt and feedback <div>s, if any
     return itemDiv;
 };
 
@@ -351,12 +409,10 @@ Instructions.prototype.createHtml = function(){
  * Screen objects constitute the basic sequence of the experiment.
  * @param type - The type of screen: Title, Instructions, or Item
  * @param object - The object with information related to the screen
- * @param next - The next screen object in the experimental sequence
  */
-function Screen(type, object, nextScreen){
+function Screen(type, object){
     this.type = type; // possible values: title, instructions, item
     this.object = object;
-    this.nextScreen = nextScreen; // Not sure this is needed
 }
 
 Screen.prototype.processKeypress = function(keyCode, elapsedTime){
@@ -381,6 +437,7 @@ function Experiment(design, form){
     this.backgroundcolor = design["background-color"] !== 'undefined' ? design["background-color"].trim() : "white";
     this.backgroundcolor = validTextColour(this.backgroundcolor) ? this.backgroundcolor : "white";
     this.display = design["display"] !== 'undefined' ? design["display"].trim() : "Moving window";
+    this.orientation = design["orientation"] !== 'undefined' ? design["orientation"] : "horizontal";
     // Following must be only one character in length
     this.fixationchar = design["fixation-character"] !== 'undefined' ? design["fixation-character"].trim().substr(0,1) : "+";
     this.maskchar = design["masking-character"] !== 'undefined' ? design["masking-character"].trim().substr(0,1) : "_";
@@ -427,8 +484,139 @@ Experiment.prototype.endExperiment = function(){
     // TODO: Optional data export of experiment log?
 };
 
-Experiment.prototype.loadStimuli = function(){
-    // TODO!
+Experiment.prototype.loadDesign = function(){
+    if (!this.designValidated){
+        if (this.design["instruction-screens"]){
+            // load pre-practice instructions
+            this.screens.concat(this.loadInstructions(this.design["instruction-screens"]));
+        }
+        if (this.design["practice-stimuli"]){
+            // load practice stimuli
+            this.screens.concat(this.loadStimuliGroup(this.design["practice-stimuli"]));
+        }
+        if (this.design["post-practice-instruction-screens"]){
+            // load post-practice instructions
+            this.screens.concat(this.loadInstructions(this.design["post-practice-instruction-screens"]));
+        }
+        if (this.design["experiment-stimuli"]){
+            // load stimulus sets
+            this.screens.concat(this.loadStimuliSets(this.design["experiment-stimuli"]));
+        }
+        if (this.design["instruction-screens"]){
+            // load ending
+            this.screens.concat(this.loadInstructions(this.design["ending-screens"]));
+        }
+    } else {
+        displayErrorMessage("Cannot load an unvalidated design: Run validateDesign() on Experiment object first.");
+        sprLog("Cannot load an unvalidated design: Run validateDesign() on Experiment object first.");
+    }
+};
+
+Experiment.prototype.loadInstructions = function(design){
+    var screens = [];
+    for (var i=0; i<design.length; i++){
+        var instructions = new Insructions(design[i]["instruction-screen"]);
+        var screen = new Screen("instructions", instructions);
+        screens.push(screen);
+    }
+    return screens;
+};
+
+Experiment.protoype.loadStimuliSets = function(design){
+    var screens = [];
+    var sets = [];
+    var order = this.getOrder(design["order"]);
+    var merge = this.getMerge(design["merge"]);
+    for (var i=0; i<design["stimuli-sets"]; i++){
+        var setDesign =design["stimuli-sets"][i]["stimuli-set"];
+        sets.push(this.loadStimuliGroups(setDesign));;
+    }
+    if (order == "random") { shuffle(sets); }
+    if (merge) {
+        screens.concat(mergeArrays(sets, "random")); // TODO: pass merge method value through
+    } else {
+        for (var k=0; k<sets.length; k++){
+            screens.concat(sets[k]);
+        }
+    }
+    return screens;
+};
+
+Experiment.protoype.loadStimuliGroups = function(design){
+    var set = [];
+    var order = this.getOrder(setDesign["order"], order);
+    var merge = this.getMerge(setDesign["merge"], merge)
+    var groups = [];
+    for (var j=0; j<design["groups"]; j++){
+        var groupDesign = design["groups"][j]["group"];
+        var group = loadStimuliGroup(groupDesign, order);
+        groups.push(group);
+    }
+    if (order == "random") {
+        shuffle(groups);
+    }
+    if (merge) {
+        set.concat(mergeArrays(groups, "random")); // TODO: pass merge method value through
+    } else {
+        for (var k=0; k<groups.length; k++){
+            set.concat(groups[k]);
+        }
+    }
+    return set;
+};
+
+Experiment.prototype.loadStimuliGroup = function(design, order){
+    var screens = [];
+    var order = this.getOrder(design["order"], order);
+    // go through items array and create screenInfo object for each item
+    for (var i=0; i<design["items"].length; i++){
+        var id = design["items"][i]["item"]["id"];
+        var text = design["items"][i]["item"]["string"];
+        var orientation = typeof design["items"][i]["item"]["orientation"] !== 'undefined' ? design["items"][i]["item"]["orientation"] : this.orientation;
+        // TODO: implement loading prompt and feedback
+        var item = new Item(id, text, orientation);
+        // Create the Screen object and push it to the sceens array
+        var screen = new Screen("stimuli", item);
+        screens.push(screen);
+    }
+    if (order === "random"){
+        shuffle(screens);
+    }
+    return screens;
+};
+
+/*
+ * Determines the "order" value or defaults to "fixed" if undefined
+ * @param   object containing "order" key-value pair
+ * @returns "fixed" or "random"
+ */
+Experiment.protype.getOrder = function(order, default){
+    var result = typeof default !=== 'undefined' ? default : "fixed"; // default
+    if (typeof order !== 'undefined'){
+        if (order["order"] === "random"){
+            result = "random";
+        } else if (order["order"] !== "fixed"){
+            sprLog("Unexpected value for 'order'. Using default 'fixed'.");
+        }
+    }
+    return result;
+};
+
+/*
+ * Determines the "merge" value or defaults to "true" if undefined
+ * @param   object containing "order" key-value pair
+ * @returns "fixed" or "random"
+ */
+Experiment.protype.getOrder = function(merge, default){
+    var result = typeof default !=== 'undefined' ? default : true; // default
+    if (typeof merge !== 'undefined'){
+        if (merge["merge"] === "false"){
+            result = false;
+        } else if (merge["merge"] !== "true"){
+            sprLog("Unexpected value for 'merge'. Using default 'true'.");
+        }
+    }
+    return result;
 };
 
 /*
@@ -444,6 +632,7 @@ Experiment.prototype.createFrame = function(){
 
 // Helper functions for validating design
 Experiment.prototype.validateDesign = function(){
+    this.designValidated = false;
     sprLog("Validating design");
     var result = true;
     // Check structure of pre-practice instructions
@@ -484,6 +673,7 @@ Experiment.prototype.validateDesign = function(){
         sprLog("No experimental stimuli section was found in the json object");
         result = false;
     }
+    this.designValidated = result;
     return result;
 };
 
@@ -674,4 +864,32 @@ function shuffle(array) {
   }
 
   return array;
+}
+
+function mergeArrays(arrays, method){
+    var result = [];
+    if (method === "random"){
+        result.concat(mergeArraysRandomly(arrays.slice(0)));
+    } else if (method == "sequential"){
+        // merge arrays in a sequential manner
+    }
+    return result;
+}
+
+function mergeArraysRandomly(arrays){
+    var result = [];
+    if (arrays.length>1) {
+        // get random array and take the top item and push to result
+        randomIndex = Math.floor(Math.random() * arrays.length);
+        result.push(arrays[randomIndex].shift());
+        return result.concat(mergeArraysRandomly(arrays.filter(function(arr){
+            return arr.length > 0;
+        })))
+    } else if (arrays.length == 1){
+        // there's only one array left, concat the whole array and stop recursion
+        return arrays[0];
+    } else {
+        // something's wrong: we should never reach here...
+    }
+    return;
 }
