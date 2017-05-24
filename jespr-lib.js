cfe85482543ae8d05acd4bef88938666b07b5640
Ralph Rose <rose@waseda.jp>
2017-05-24 16:42:48 +0100

    updated jespr-lib.js
    Added code for array.filter() method for IE<9 compatibility

--- jespr-lib.js
+++ jespr-lib.js
@@ -32,6 +32,45 @@ if (!Array.prototype.some)
 }
 
 /*
+ * This provides the Array.filter() method for IE<9. Taken from
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter?v=control
+ */
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun/*, thisArg*/) {
+    'use strict';
+
+    if (this === void 0 || this === null) {
+      throw new TypeError();
+    }
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+
+        // NOTE: Technically this should Object.defineProperty at
+        //       the next index, as push can be affected by
+        //       properties on Object.prototype and Array.prototype.
+        //       But that method's new, and collisions should be
+        //       rare, so use the more-compatible alternative.
+        if (fun.call(thisArg, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+}
+
+/*
  * The Region object defines a region of text to be displayed in
  * a self-paced-reading experiment.
  * @param id - The ID for this region
